65a659fb444de13798b52575b09a52015d69bc8c
Alice Won <alicewon4@gmail.com>
2020-02-21 15:52:50 -0800

    testing

---
+++ app/channels/drawings_channel.rb
@@ -0,0 +1,10 @@
+class DrawingsChannel < ApplicationCable::Channel
+  def subscribed
+    # stream_from "some_channel"
+    stream_from "drawings_channel"
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   resources :player_rounds
   resources :rounds
   resources :players
+  
+  mount ActionCable.server => '/cable'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
