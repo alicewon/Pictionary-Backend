7bd52f703a299a23daa515d11d44ee0cecef5d38
Alice Won <alicewon4@gmail.com>
2020-02-22 01:24:04 -0800

    trying websocket things

--- app/channels/drawings_channel.rb
+++ app/channels/drawings_channel.rb
@@ -4,6 +4,10 @@ class DrawingsChannel < ApplicationCable::Channel
     stream_from "drawings_channel"
   end
 
+  def received(data)
+    DrawingsChannel.braodcast_to(@room, {room: @room, users: @room.users, messages: @room.messages})
+  end
+
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
   end
