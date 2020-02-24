7ac3f21a16a8a0ecbf168baa63a5685517c176cd
Alice Won <alicewon4@gmail.com>
2020-02-24 01:43:43 -0800

    not sure what i did here, just fine tuning web socket stuff

--- app/channels/drawings_channel.rb
+++ app/channels/drawings_channel.rb
@@ -4,9 +4,16 @@ class DrawingsChannel < ApplicationCable::Channel
     stream_from "drawings_channel"
   end
 
+  # jennifer ingram blog:
   # def received(data)
   #   DrawingsChannel.braodcast_to(@room, {room: @room, users: @room.users, messages: @room.messages})
   # end
+
+  # christian blog:
+  # def receive(data)
+  #   ActionCable.server.broadcast("drawings_channel#{data['drawing_id']}", data)
+  # end
+
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
--- app/controllers/drawings_controller.rb
+++ app/controllers/drawings_controller.rb
@@ -1,2 +1,7 @@
 class DrawingsController < ApplicationController
+  #trying this from paint rails article: https://medium.com/@hdwatts/creating-a-drawing-with-friends-web-app-w-action-cable-and-rails-5-1052ac43d74b
+  def show
+    ActionCable.server.broadcast 'drawings_channel',
+    head :ok
+  end
 end
