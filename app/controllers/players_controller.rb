75c00d1833c79693fc5686698c4b598548c8d15f
Alice Won <alicewon4@gmail.com>
2020-03-01 22:34:30 -0800

    updated players controller

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,2 +1,21 @@
 class PlayersController < ApplicationController
+
+  def index
+    players = Player.all
+    render json: players
+  end
+
+  def show
+    player = Player.find_by(username: params[:id])
+    render :json => players
+  end
+
+  def create
+    player = Player.find_or_create_by(params.require(:player).permit(:username))
+    if player.save
+      render :json => owner
+    else
+      flash[:message]= player.errors.full_messages
+    end
+  end
 end
